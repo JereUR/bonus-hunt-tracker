f30d7a541e98f7c091789a086a184cb15c5d7669
JereUR <jeremias.jdv@gmail.com>
2023-09-20 04:42:34 -0300

    fix(Root): Styles fixes

--- src/components/Home/RightSide.jsx
+++ src/components/Home/RightSide.jsx
@@ -7,8 +7,7 @@ export default function RightSide() {
 const RightSideStyled = styled.div`
   flex: 1;
   background: rgba(10, 10, 10, 0.78);
-  border: 3px solid var(--primary-color);
-  border-left-width: 0px;
+  border: 2px solid var(--primary-color);
   backdrop-filter: blur(4.5px);
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
