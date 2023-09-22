82b6564b6df68739fd0a3607a6d04b3d18fed7cc
JereUR <jeremias.jdv@gmail.com>
2023-09-22 01:35:32 -0300

    feat(Root): Initial data for bonus list

--- src/components/Home/LeftSide.jsx
+++ src/components/Home/LeftSide.jsx
@@ -11,11 +11,74 @@ const initialBonusData = {
   bet: ''
 }
 
+const data = [
+  {
+    id: 'bonus_1000',
+    slotName: 'Slot 1',
+    bet: '30'
+  },
+  {
+    id: 'bonus_2000',
+    slotName: 'Slot 2',
+    bet: '40'
+  },
+  {
+    id: 'bonus_3000',
+    slotName: 'Slot 3',
+    bet: '50'
+  },
+  {
+    id: 'bonus_4000',
+    slotName: 'Slot 4',
+    bet: '30'
+  },
+  {
+    id: 'bonus_5000',
+    slotName: 'Slot 5',
+    bet: '80'
+  },
+  {
+    id: 'bonus_6000',
+    slotName: 'Slot 6',
+    bet: '60'
+  },
+  {
+    id: 'bonus_7000',
+    slotName: 'Slot 7',
+    bet: '70'
+  },
+  {
+    id: 'bonus_8000',
+    slotName: 'Slot 8',
+    bet: '80'
+  },
+  {
+    id: 'bonus_9000',
+    slotName: 'Slot 9',
+    bet: '90'
+  },
+  {
+    id: 'bonus_1100',
+    slotName: 'Slot 10',
+    bet: '130'
+  },
+  {
+    id: 'bonus_1200',
+    slotName: 'Slot 11',
+    bet: '40'
+  },
+  {
+    id: 'bonus_1300',
+    slotName: 'Slot 12',
+    bet: '50'
+  }
+]
+
 export default function LeftSide({ onRun, setOnRun }) {
   const [budget, setBudget] = useState('')
   const [classNameBudget, setClassNameBudget] = useState(false)
   const [checked, setChecked] = useState(false)
-  const [bonusList, setBonusList] = useState([])
+  const [bonusList, setBonusList] = useState(data)
   const [bonusData, setBonusData] = useState(initialBonusData)
   const [classNameSlotName, setClassNameSlotName] = useState(false)
   const [classNameBet, setClassNameBet] = useState(false)
@@ -205,6 +268,7 @@ const LeftSideStyled = styled.div`
   }
 
   .submit-btn {
+    bottom: 10px;
     cursor: pointer;
     font-size: 24px;
     font-weight: bold;
