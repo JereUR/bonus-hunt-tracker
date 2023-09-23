876080d9caefeeb819813213f132765e428f0e41
JereUR <jeremias.jdv@gmail.com>
2023-09-22 22:15:36 -0300

    feat(Root): Now onRun state came from global context

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components'
+
 import LeftSide from './LeftSide'
 import RightSide from './RightSide'
-import { useState } from 'react'
 
 export default function Home() {
-  const [onRun, setOnRun] = useState(false)
-
   return (
     <HomeStyled>
-      <LeftSide onRun={onRun} setOnRun={setOnRun} />
-      <RightSide onRun={onRun} />
+      <LeftSide />
+      <RightSide />
     </HomeStyled>
   )
 }
