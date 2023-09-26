02ec96da4409b85eec9cf35af0ade5ff19d976be
JereUR <jeremias.jdv@gmail.com>
2023-09-25 22:12:07 -0300

    fix(Root): Fix values of initial data

--- src/context/globalContext.jsx
+++ src/context/globalContext.jsx
@@ -5,82 +5,94 @@ const costumBonusList = [
   {
     id: 'bonus_1000',
     slotName: 'Slot 1',
-    bet: '30',
-    win: null
+    bet: 30,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_2000',
     slotName: 'Slot 2',
-    bet: '40',
-    win: null
+    bet: 40,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_3000',
     slotName: 'Slot 3',
-    bet: '50',
-    win: null
+    bet: 50,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_4000',
     slotName: 'Slot 4',
-    bet: '30',
-    win: null
+    bet: 30,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_5000',
     slotName: 'Slot 5',
-    bet: '80',
-    win: null
+    bet: 80,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_6000',
     slotName: 'Slot 6',
-    bet: '60',
-    win: null
+    bet: 60,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_7000',
     slotName: 'Slot 7',
-    bet: '70',
-    win: null
+    bet: 70,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_8000',
     slotName: 'Slot 8',
-    bet: '80',
-    win: null
+    bet: 80,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_9000',
     slotName: 'Slot 9',
-    bet: '90',
-    win: null
+    bet: 90,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_1100',
     slotName: 'Slot 10',
-    bet: '130',
-    win: null
+    bet: 130,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_1200',
     slotName: 'Slot 11',
-    bet: '40',
-    win: null
+    bet: 40,
+    win: null,
+    odd: null
   },
   {
     id: 'bonus_1300',
     slotName: 'Slot 12',
-    bet: '50',
-    win: null
+    bet: 50,
+    win: null,
+    odd: null
   }
 ]
 
 const GlobalContext = createContext()
 
 export const GlobalProvider = ({ children }) => {
-  const [onRun, setOnRun] = useState(true)
-  const [budget, setBudget] = useState('')
+  const [onRun, setOnRun] = useState(false)
+  const [budget, setBudget] = useState(null)
   const [bonusList, setBonusList] = useState(costumBonusList)
 
   const deleteBonusItem = (id) => {
