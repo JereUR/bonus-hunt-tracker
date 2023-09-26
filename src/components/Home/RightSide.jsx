80deddcc89b8cdeeafe032be845e5a1ed9917647
JereUR <jeremias.jdv@gmail.com>
2023-09-25 22:11:36 -0300

    feat(Root) Stats componet

--- src/components/Home/RightSide.jsx
+++ src/components/Home/RightSide.jsx
@@ -1,10 +1,43 @@
 import styled from 'styled-components'
 import { useGlobalContext } from '../../context/globalContext'
+import useGetAttributesDetails from '../../utils/useGetAttributesDetails'
 
 export default function RightSide() {
-  const { budget, bonusList, onRun } = useGlobalContext()
+  const { budget, bonusList } = useGlobalContext()
+  const { maxWin, maxOdd, currentAvg, requiredAvg } = useGetAttributesDetails()
+  console.log({ maxWin })
+  console.log({ maxOdd })
+  console.log({ currentAvg })
+  console.log({ requiredAvg })
 
-  return <RightSideStyled>{budget}</RightSideStyled>
+  return (
+    <RightSideStyled>
+      <div className="data-container">
+        <h5>START:</h5>
+        <p>{budget ? budget : '-'}</p>
+      </div>
+      <div className="data-container">
+        <h5>CURRENT AVG:</h5>
+        <p>{currentAvg ? currentAvg : '-'}</p>
+      </div>
+      <div className="data-container">
+        <h5>REQUIRED AVG :</h5>
+        <p>{requiredAvg ? requiredAvg : '-'}</p>
+      </div>
+      <div className="data-container">
+        <h5>BONUSES:</h5>
+        <p>{bonusList.length}</p>
+      </div>
+      <div className="data-container">
+        <h5>MAX WIN:</h5>
+        <p>{maxWin ? maxWin : '-'}</p>
+      </div>
+      <div className="data-container">
+        <h5>MAX X:</h5>
+        <p>{maxOdd ? maxOdd : '-'}</p>
+      </div>
+    </RightSideStyled>
+  )
 }
 
 const RightSideStyled = styled.div`
@@ -17,4 +50,13 @@ const RightSideStyled = styled.div`
   overflow: auto;
   overflow-x: hidden;
   overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--primary-color3);
+    border-radius: 5px;
+  }
 `
