3fa3ac01b8e004a217e6165a4d70817e86f81127
JereUR <jeremias.jdv@gmail.com>
2023-09-27 21:36:07 -0300

    feat(Root): Styles added

--- src/components/Home/RightSide.jsx
+++ src/components/Home/RightSide.jsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
 import { useGlobalContext } from '../../context/globalContext'
 import useGetAttributesDetails from '../../utils/useGetAttributesDetails'
 
@@ -12,15 +13,15 @@ export default function RightSide() {
   return (
     <RightSideStyled>
       <div className="data-container">
-        <h5>START:</h5>
+        <h3>START</h3>
         <p>{budget ? `$${budget}` : '-'}</p>
       </div>
       <div className="data-container">
-        <h5>CURRENT AVG:</h5>
+        <h3>CURRENT AVG</h3>
         <p>{currentAvg ? `x ${currentAvg}` : '-'}</p>
       </div>
       <div className="data-container">
-        <h5>REQUIRED AVG :</h5>
+        <h3>REQUIRED AVG</h3>
         {requiredAvg ? (
           <p>{requiredAvg <= 0 ? 'PROFIT' : `x ${requiredAvg}`}</p>
         ) : (
@@ -28,23 +29,23 @@ export default function RightSide() {
         )}
       </div>
       <div className="data-container">
-        <h5>BONUSES:</h5>
+        <h3>BONUSES</h3>
         <p>{bonusList.length}</p>
       </div>
       <div className="data-container">
-        <h5>MAX WIN:</h5>
+        <h3>MAX WIN</h3>
         <p>{maxWin ? `$${maxWin}` : '-'}</p>
       </div>
       <div className="data-container">
-        <h5>MAX X:</h5>
+        <h3>MAX X</h3>
         <p>{maxOdd ? `x ${maxOdd}` : '-'}</p>
       </div>
       <div className="data-container">
-        <h5>TOTAL WIN:</h5>
+        <h3>TOTAL WIN</h3>
         <p>{totalWin ? `$${totalWin}` : '-'}</p>
       </div>
-      <div className="data-container">
-        <h5>BALANCE:</h5>
+      <div className="data-container balance ">
+        <h2>BALANCE</h2>
         <p className={finalResult < 0 ? 'loss' : 'profit'}>
           {finalResult > 0 ? `+ $${finalResult}` : `$${finalResult}`}
         </p>
@@ -52,6 +53,24 @@ export default function RightSide() {
     </RightSideStyled>
   )
 }
+
+const rotateBorder = keyframes`
+  0% {
+    border-image-source: linear-gradient(to right, var(--secondary-color), var(--primary-color));
+  }
+  25% {
+    border-image-source: linear-gradient(to bottom, var(--secondary-color), var(--primary-color));
+  }
+  50% {
+    border-image-source: linear-gradient(to left, var(--secondary-color), var(--primary-color));
+  }
+  75% {
+    border-image-source: linear-gradient(to top, var(--secondary-color), var(--primary-color));
+  }
+  100% {
+    border-image-source: linear-gradient(to right, var(--secondary-color), var(--primary-color));
+  }
+`
 
 const RightSideStyled = styled.div`
   flex: 1;
@@ -64,13 +83,75 @@ const RightSideStyled = styled.div`
   overflow-x: hidden;
   overflow-y: auto;
 
-  .data-container {
+  .balance {
+    margin: 40px 15px 40px 15px !important;
+
+    p {
+      font-size: 26px !important;
+    }
+
     .loss {
       color: var(--error-color);
     }
 
     .profit {
       color: var(--check-color);
+    }
+  }
+
+  .data-container {
+    padding: 5px;
+    margin: 20px 50px;
+    border-radius: 10px;
+    background-color: var(--background-home-color);
+    border: 1px solid;
+    border-image-slice: 1;
+    animation: ${rotateBorder} 8s infinite linear;
+    line-height: 5px;
+
+    h2 {
+      position: relative;
+      display: inline-block;
+      letter-spacing: 8px;
+      color: var(--secondary-color);
+      text-shadow: 3px 3px 5px var(--primary-color3);
+
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: -20px;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background-color: var(--secondary-color);
+        z-index: -1;
+        text-shadow: inherit;
+      }
+    }
+
+    h3 {
+      position: relative;
+      display: inline-block;
+      letter-spacing: 2px;
+      color: var(--secondary-color);
+      text-shadow: 3px 3px 5px var(--primary-color3);
+
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: -10px;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background-color: var(--secondary-color);
+        z-index: -1;
+        text-shadow: inherit;
+      }
+    }
+
+    p {
+      font-size: 20px;
+      font-weight: bold;
     }
   }
 
