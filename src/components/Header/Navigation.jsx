43be9f41a102df2748d9b3c91997a5585f25a06e
JereUR <jeremias.jdv@gmail.com>
2023-10-03 02:06:03 -0300

    feat(Root): Styles added

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,41 +1,75 @@
 import styled from 'styled-components'
-import PropTypes from 'prop-types'
 
 import { menuItems } from '../../utils/menuItmes'
 import { logoutIcon } from '../../utils/Icons'
 import { signOutFromSupabase } from '../../services'
-import { useGlobalContext } from '../../context/globalContext'
 
 export default function Navigation({ active, setActive }) {
-  const { session } = useGlobalContext()
-
   return (
     <NavStyled>
       <div className="menu-items">
-        {menuItems.map((item) => {
-          return (
-            <p
-              key={item.id}
-              onClick={() => {
-                if (session !== null && session?.session !== null)
-                  setActive(item.id)
-              }}
-              className={active === item.id ? 'active' : ''}
-            >
-              {item.icon}
-              <span>{item.title}</span>
-            </p>
-          )
-        })}
-      </div>
-      <div className="bottom-nav">
-        <p onClick={() => signOutFromSupabase()}>{logoutIcon} Cerrar Sesión</p>
+        {menuItems.map((item) => (
+          <div
+            key={item.id}
+            onClick={() => {
+              setActive(item.id)
+            }}
+            className={active === item.id ? 'btn-nav active' : 'btn-nav'}
+          >
+            {item.icon}
+            <span>{item.title}</span>
+          </div>
+        ))}
+        <div className="btn-nav logout" onClick={() => signOutFromSupabase()}>
+          <span>{logoutIcon} Cerrar Sesión</span>
+        </div>
       </div>
     </NavStyled>
   )
 }
 
 const NavStyled = styled.nav`
-  display: flex;
-  justify-content: space-between;
+  margin-top: 50px;
+  margin-bottom: -30px;
+
+  .btn-nav {
+    z-index: 2;
+    cursor: pointer;
+  }
+
+  .menu-items {
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+    line-height: 40px;
+    transition: all 0.3s ease-in-out;
+
+    .active {
+      border-bottom: 3px solid var(--primary-color);
+      border-radius: 7px;
+    }
+  }
+
+  span {
+    font-weight: bold;
+    font-size: 18px;
+    padding: 10px;
+
+    .ti,
+    i {
+      font-size: 18px !important;
+    }
+  }
+
+  .logout {
+    position: relative;
+    right: -100px;
+    border: 1px solid var(--primary-color);
+    border-radius: 10px;
+    transition: all 0.4s ease-in-out;
+
+    &:hover {
+      color: var(--primary-color);
+    }
+  }
 `
