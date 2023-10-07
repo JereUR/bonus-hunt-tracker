0777cbd9a2c79f23b9046cc4fb07ec547d2756cc
JereUR <jeremias.jdv@gmail.com>
2023-10-06 22:12:35 -0300

    feat(Root): Components for recover password added

---
+++ src/components/Recover/EmailForm.jsx
@@ -0,0 +1,427 @@
+import { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+import { toast } from 'react-toastify'
+
+import newEmail from '../../assets/newEmail.png'
+import { arrowBack } from '../../utils/Icons'
+import { DIRECTIONS } from '../../utils/Direction'
+import Loader from '../Loader/Loader'
+import { recoverPassword } from '../../services'
+
+const validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+export default function EmailForm({ setActive }) {
+  const [email, setEmail] = useState('')
+  const [classNameEmail, setClassNameEmail] = useState(false)
+  const [errorForm, setErrorForm] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [recSent, setRecSent] = useState(false)
+
+  const handleBlur = () => {
+    if (email === '') {
+      setClassNameEmail(false)
+    }
+  }
+
+  const validation = () => {
+    let err = ''
+
+    if (email === '') {
+      err = 'Debe ingresar un email.'
+    }
+
+    if (!validateEmail.test(String(email).toLowerCase())) {
+      err = 'Email no válido.'
+    }
+
+    return err
+  }
+
+  const handleRecover = async (e) => {
+    e.preventDefault()
+
+    const err = validation()
+    setErrorForm(err)
+    setRecSent(true)
+
+    if (Object.keys(err).length === 0) {
+      setLoading(true)
+      const { error } = await recoverPassword(email)
+
+      if (error) {
+        toast.error(error.message, {
+          position: 'top-right',
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark'
+        })
+      } else {
+        setEmail('')
+        setRecSent(true)
+      }
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <EmailFormStyled>
+      <ButtonBack onClick={() => setActive(DIRECTIONS.LOGIN)}>
+        {arrowBack} <span>Volver al inicio</span>
+      </ButtonBack>
+      <h1>Recuperar Contraseña</h1>
+      {recSent ? (
+        <div className="recover-sent">
+          <div className="img-container">
+            <img src={newEmail} alt="new-email" />
+          </div>
+          <p>
+            <strong>
+              Se ha enviado en correo electrónico con instrucciones para
+              reestablecer la contraseña.
+            </strong>
+          </p>
+        </div>
+      ) : (
+        <form onSubmit={handleRecover}>
+          <div className="user-box">
+            <input
+              className={classNameEmail ? 'focus' : ''}
+              onFocus={() => setClassNameEmail(true)}
+              onBlur={handleBlur}
+              value={email}
+              name="email"
+              type="text"
+              onChange={({ target }) => {
+                setEmail(target.value)
+              }}
+            />
+            {errorForm && <ErrorText>{errorForm}</ErrorText>}
+            <label>Email</label>
+          </div>
+          <button className="recover-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Enviar
+          </button>
+          {loading && <Loader customClass="recover-loader" />}
+        </form>
+      )}
+    </EmailFormStyled>
+  )
+}
+
+const btnAnim1 = keyframes`
+  0% {
+    left: -100%;
+  }
+
+  50%,100% {
+    left: 100%;
+  }
+`
+
+const btnAnim2 = keyframes`
+  0% {
+    top: -100%;
+  }
+
+  50%,
+  100% {
+    top: 100%;
+  }
+`
+
+const btnAnim3 = keyframes`
+  0% {
+    right: -100%;
+  }
+
+  50%,
+  100% {
+    right: 100%;
+  }
+`
+
+const btnAnim4 = keyframes`
+  0% {
+    bottom: -100%;
+  }
+
+  50%,
+  100% {
+    bottom: 100%;
+  }
+`
+
+const shake = keyframes`
+  0%, 100% {
+    transform: translateX(0);
+  }
+  20%, 60%{
+    transform: translateX(-5px);
+  }
+  40%, 80% {
+    transform: translateX(5px);
+  }
+  `
+
+const ButtonBack = styled.button`
+  cursor: pointer;
+  background: none;
+  border: none;
+  position: absolute;
+  top: 5%;
+  left: 6%;
+  transition: all 0.3s ease-in-out;
+
+  span {
+    position: absolute;
+    top: 17%;
+    font-size: 18px;
+    width: 10vw;
+    margin-left: 10px;
+    font-weight: bold;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    color: var(--primary-color);
+  }
+`
+
+const ErrorText = styled.span`
+  display: flex;
+  justify-content: left;
+  font-size: 12px;
+  color: var(--error-color);
+  animation: ${shake} 0.6s;
+`
+
+const EmailFormStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 500px;
+  height: 60vh;
+  padding: 100px 60px 40px 60px;
+  margin: 20px auto;
+  transform: translate(-50%, -55%);
+  background: rgba(0, 0, 0, 0.9);
+  border: 3px solid var(--primary-color);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+  overflow-y: auto;
+
+  h1 {
+    color: var(--primary-color);
+    font-size: 36px;
+    margin-bottom: 70px;
+  }
+
+  .recover-loader {
+    margin: 10px auto;
+  }
+
+  .recover-sent {
+    .img-container {
+      margin-top: -30px;
+
+      img {
+        width: 130px;
+      }
+    }
+
+    strong {
+      font-size: 16px;
+    }
+  }
+
+  p:first-child {
+    margin: 0 0 30px;
+    padding: 0;
+    color: #fff;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+
+  .user-box {
+    position: relative;
+  }
+
+  .user-box input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+  }
+
+  .user-box label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: 0.5s;
+  }
+
+  .user-box .focus ~ label {
+    top: -25px;
+    left: 0;
+    color: var(--primary-color2);
+    font-size: 14px;
+  }
+
+  .user-box .focus {
+    border-bottom: 1px solid var(--primary-color);
+    color: var(--primary-color);
+  }
+
+  .recover-btn {
+    position: relative;
+    display: inline-block;
+    padding: 15px 20px;
+    width: 60%;
+    font-weight: bold;
+    background: transparent;
+    cursor: pointer;
+    border: none;
+    color: var(--primary-color);
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    overflow: hidden;
+    transition: 0.5s;
+    margin: 40px 0 10px 0;
+    letter-spacing: 3px;
+    transition: all 0.4s ease-in-out;
+
+    &::before {
+      content: '';
+      width: 0%;
+      height: 100%;
+      position: absolute;
+      background-color: var(--primary-color);
+      left: 0;
+      top: 0;
+      z-index: -1;
+      transition: 0.4s ease-in-out;
+    }
+
+    &:hover {
+      border-radius: 5px;
+      color: #fff;
+    }
+
+    &:hover::before {
+      content: '';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+    }
+
+    span {
+      position: absolute;
+      display: block;
+    }
+
+    span:nth-child(1) {
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(90deg, transparent, var(--primary-color));
+      animation: ${btnAnim1} 1.5s linear infinite;
+    }
+
+    span:nth-child(1) {
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(90deg, transparent, var(--primary-color));
+      animation: ${btnAnim1} 1.5s linear infinite;
+    }
+
+    span:nth-child(2) {
+      top: -100%;
+      right: 0;
+      width: 2px;
+      height: 100%;
+      background: linear-gradient(180deg, transparent, var(--primary-color));
+      animation: ${btnAnim2} 1.5s linear infinite;
+      animation-delay: 0.375s;
+    }
+
+    span:nth-child(3) {
+      bottom: 0;
+      right: -100%;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(270deg, transparent, var(--primary-color));
+      animation: ${btnAnim3} 1.5s linear infinite;
+      animation-delay: 0.75s;
+    }
+
+    span:nth-child(4) {
+      bottom: -100%;
+      left: 0;
+      width: 2px;
+      height: 100%;
+      background: linear-gradient(360deg, transparent, var(--primary-color));
+      animation: ${btnAnim4} 1.5s linear infinite;
+      animation-delay: 1.125s;
+    }
+  }
+
+  .signup {
+    display: flex;
+    justify-content: center;
+    margin-bottom: -25px;
+  }
+
+  p:last-child {
+    color: #aaa;
+    font-size: 14px;
+  }
+
+  p.a2 {
+    cursor: pointer;
+    margin-left: 5px;
+    color: #fff;
+    text-decoration: none;
+  }
+
+  p.a2:hover {
+    background: transparent;
+    color: #aaa;
+    border-radius: 5px;
+  }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--primary-color3);
+    border-radius: 5px;
+  }
+`
---
+++ src/components/Recover/PasswordForm.jsx
@@ -0,0 +1,414 @@
+import { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+import { toast } from 'react-toastify'
+
+import { updateUserPassword } from '../../services'
+import { DIRECTIONS } from '../../utils/Direction'
+import Loader from '../Loader/Loader'
+
+const initialState = {
+  password: '',
+  confirm_password: ''
+}
+
+export default function PasswordForm({ setActive }) {
+  const [inputState, setInputState] = useState(initialState)
+  const [classNamePassword, setClassNamePassword] = useState(false)
+  const [classNamePasswordConfirm, setClassNamePasswordConfirm] =
+    useState(false)
+  const [errorForm, setErrorForm] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const { password, confirm_password } = inputState
+
+  const handleFocus = (nameTag) => {
+    switch (nameTag) {
+      case 'password':
+        setClassNamePassword(true)
+        break
+      case 'confirm_password':
+        setClassNamePasswordConfirm(true)
+        break
+    }
+  }
+
+  const handleBlur = (nameTag) => {
+    switch (nameTag) {
+      case 'password':
+        if (password === '') {
+          setClassNamePassword(false)
+        }
+        break
+      case 'confirm_password':
+        if (confirm_password === '') {
+          setClassNamePasswordConfirm(false)
+        }
+        break
+    }
+  }
+
+  const validation = () => {
+    let err = {}
+
+    if (password === '') {
+      err.password = 'Debe ingresar una contraseña.'
+    }
+
+    if (password.length < 8) {
+      err.password = 'La contraseña debe tener un mínimo de 8 caracteres.'
+    }
+
+    if (confirm_password === '') {
+      err.confirm_password = 'Debe confirmar su contraseña.'
+    }
+
+    if (confirm_password !== password) {
+      err.password = 'Las contraseñas no coinciden.'
+      err.confirm_password = 'Las contraseñas no coinciden.'
+    }
+
+    return err
+  }
+
+  const handleInput =
+    (name) =>
+    ({ target }) => {
+      setInputState({ ...inputState, [name]: target.value })
+    }
+
+  const handleRecover = async (e) => {
+    e.preventDefault()
+
+    const err = validation()
+    setErrorForm(err)
+
+    if (Object.keys(err).length === 0) {
+      setLoading(true)
+      const { error } = await updateUserPassword({ password })
+
+      if (error) {
+        toast.error(error.message, {
+          position: 'top-right',
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark'
+        })
+      } else {
+        toast.success('Contraseña reestablecida. Redirigiendo...', {
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark'
+        })
+
+        setTimeout(() => {
+          setInputState(initialState)
+          setActive(DIRECTIONS.HOME)
+          window.location.href = '/'
+        }, 2000)
+      }
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <PasswordFormStyled>
+      <h1>Actualiza tu Contraseña</h1>
+      <form onSubmit={handleRecover}>
+        <div className="user-box">
+          <input
+            className={classNamePassword ? 'focus' : ''}
+            onFocus={({ target }) => handleFocus(target.name)}
+            onBlur={({ target }) => handleBlur(target.name)}
+            value={password}
+            name="password"
+            type="password"
+            onChange={handleInput('password')}
+          />
+          {errorForm.password && <ErrorText>{errorForm.password}</ErrorText>}
+          <label>Contraseña</label>
+        </div>
+        <div className="user-box">
+          <input
+            className={classNamePasswordConfirm ? 'focus' : ''}
+            onFocus={({ target }) => handleFocus(target.name)}
+            onBlur={({ target }) => handleBlur(target.name)}
+            value={confirm_password}
+            name="confirm_password"
+            type="password"
+            onChange={handleInput('confirm_password')}
+          />
+          {errorForm.confirm_password && (
+            <ErrorText>{errorForm.confirm_password}</ErrorText>
+          )}
+          <label>Confirmar contraseña</label>
+        </div>
+        <button className="recover-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Enviar
+        </button>
+        {loading && <Loader customClass="recover-loader" />}
+      </form>
+    </PasswordFormStyled>
+  )
+}
+
+const btnAnim1 = keyframes`
+  0% {
+    left: -100%;
+  }
+
+  50%,100% {
+    left: 100%;
+  }
+`
+
+const btnAnim2 = keyframes`
+  0% {
+    top: -100%;
+  }
+
+  50%,
+  100% {
+    top: 100%;
+  }
+`
+
+const btnAnim3 = keyframes`
+  0% {
+    right: -100%;
+  }
+
+  50%,
+  100% {
+    right: 100%;
+  }
+`
+
+const btnAnim4 = keyframes`
+  0% {
+    bottom: -100%;
+  }
+
+  50%,
+  100% {
+    bottom: 100%;
+  }
+`
+
+const shake = keyframes`
+  0%, 100% {
+    transform: translateX(0);
+  }
+  20%, 60%{
+    transform: translateX(-5px);
+  }
+  40%, 80% {
+    transform: translateX(5px);
+  }
+  `
+
+const ErrorText = styled.span`
+  display: flex;
+  justify-content: left;
+  font-size: 12px;
+  margin-top: -10px;
+  margin-bottom: 5px;
+  color: var(--error-color);
+  animation: ${shake} 0.6s;
+`
+
+const PasswordFormStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 500px;
+  height: 60vh;
+  padding: 100px 60px 40px 60px;
+  margin: 20px auto;
+  transform: translate(-50%, -55%);
+  background: rgba(0, 0, 0, 0.9);
+  border: 3px solid var(--primary-color);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+  overflow-y: auto;
+
+  h1 {
+    color: var(--primary-color);
+    font-size: 32px;
+    margin-bottom: 70px;
+  }
+
+  .recover-loader {
+    margin: 10px auto;
+  }
+
+  p:first-child {
+    margin: 0 0 30px;
+    padding: 0;
+    color: #fff;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+
+  .user-box {
+    position: relative;
+  }
+
+  .user-box input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+  }
+
+  .user-box label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: 0.5s;
+  }
+
+  .user-box .focus ~ label {
+    top: -25px;
+    left: 0;
+    color: var(--primary-color2);
+    font-size: 14px;
+  }
+
+  .user-box .focus {
+    border-bottom: 1px solid var(--primary-color);
+    color: var(--primary-color);
+  }
+
+  .recover-btn {
+    position: relative;
+    display: inline-block;
+    padding: 15px 20px;
+    font-weight: bold;
+    background: transparent;
+    cursor: pointer;
+    border: none;
+    color: var(--primary-color);
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    overflow: hidden;
+    transition: 0.5s;
+    margin: 40px 0 10px 0;
+    letter-spacing: 3px;
+    transition: all 0.4s ease-in-out;
+
+    &::before {
+      content: '';
+      width: 0%;
+      height: 100%;
+      position: absolute;
+      background-color: var(--primary-color);
+      left: 0;
+      top: 0;
+      z-index: -1;
+      transition: 0.4s ease-in-out;
+    }
+
+    &:hover {
+      border-radius: 5px;
+      color: #fff;
+    }
+
+    &:hover::before {
+      content: '';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+    }
+
+    span {
+      position: absolute;
+      display: block;
+    }
+
+    span:nth-child(1) {
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(90deg, transparent, var(--primary-color));
+      animation: ${btnAnim1} 1.5s linear infinite;
+    }
+
+    span:nth-child(1) {
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(90deg, transparent, var(--primary-color));
+      animation: ${btnAnim1} 1.5s linear infinite;
+    }
+
+    span:nth-child(2) {
+      top: -100%;
+      right: 0;
+      width: 2px;
+      height: 100%;
+      background: linear-gradient(180deg, transparent, var(--primary-color));
+      animation: ${btnAnim2} 1.5s linear infinite;
+      animation-delay: 0.375s;
+    }
+
+    span:nth-child(3) {
+      bottom: 0;
+      right: -100%;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(270deg, transparent, var(--primary-color));
+      animation: ${btnAnim3} 1.5s linear infinite;
+      animation-delay: 0.75s;
+    }
+
+    span:nth-child(4) {
+      bottom: -100%;
+      left: 0;
+      width: 2px;
+      height: 100%;
+      background: linear-gradient(360deg, transparent, var(--primary-color));
+      animation: ${btnAnim4} 1.5s linear infinite;
+      animation-delay: 1.125s;
+    }
+  }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--primary-color3);
+    border-radius: 5px;
+  }
+`
